b2c4e058ee36e0855cb8404c73174807a8f83ea6
vcordero07 <vcordero07@gmail.com>
2022-04-10 22:13:14 -0400

    navbar and toggle menu set

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 // Grab elements
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if (element) return element;
+    throw new Error(`something went wrong, make sure that ${selector} exists or is typed correctly.`)
+}
 
 //Nav styles on scroll
 
-// Open menu & search pop-up
+const scrollHeader = () =>{
+    const navbarElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        navbarElement.classList.add('activated');
+    } else {
+        navbarElement.classList.remove('activated');
+    }
+}
 
+window.addEventListener('scroll', scrollHeader);
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
